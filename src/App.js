e0b7648d1b75f4a0b3c19930b836ab0bbdc34520
IvanGML <somovlife@gmail.com>
2019-03-24 16:24:35 +0300

    add list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Navbar from "./navbar-transition/Navbar";
+// import Navbar from "./navbar-transition/Navbar";
+import List from "./list-transition/List";
 
 class App extends Component {
   render() {
     return (
-      <Navbar />
+      <List />
     );
   }
 }
