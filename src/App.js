a65a78b7467bc2fe61ed5a7559f1ae6a3a0f7395
IvanGML <somovlife@gmail.com>
2019-03-24 15:24:35 +0300

    add navbar component with swipe transition to top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Navbar from "./navbar-transition/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
-      </div>
+      <Navbar />
     );
   }
 }
---
+++ src/navbar-transition/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { CSSTransition } from 'react-transition-group';
+import cx from 'classnames';
+import './navbar.css';
+
+class Navbar extends Component {
+    state = {
+        showBalloon: false,
+    };
+
+    toggle = () => {
+        this.setState(prevState => ({
+            showBalloon: !prevState.showBalloon,
+        }));
+    };
+
+    render() {
+        const { showBalloon } = this.state;
+        return (
+            <div className="container">
+                <button
+                    className={
+                        cx('toggler',
+                            {
+                                'toggler--active': this.state.showBalloon,
+                            }
+                        )}
+                    onClick={this.toggle}
+                >
+                    Menu
+                </button>
+                <CSSTransition
+                    in={showBalloon}
+                    timeout={400}
+                    classNames="balloon"
+                    unmountOnExit
+                    >
+                    <div className="menu">
+                        <ul className="list">
+                            <li className="list-item">Home</li>
+                            <li className="list-item">Profile</li>
+                            <li className="list-item">Favorites</li>
+                            <li className="list-item">Sign out</li>
+                        </ul>
+                    </div>
+                </CSSTransition>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
